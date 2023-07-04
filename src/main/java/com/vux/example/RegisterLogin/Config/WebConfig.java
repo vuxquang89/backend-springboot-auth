ce081b90c840eebaf81513c0e521eaedeca6a62f
vuxquang89 <vux.quang89@gmail.com>
2023-07-05 01:22:00 +0700

    set up webconfig enable upload file

--- src/main/java/com/vux/example/RegisterLogin/Config/WebConfig.java
+++ src/main/java/com/vux/example/RegisterLogin/Config/WebConfig.java
@@ -1,9 +1,13 @@
 package com.vux.example.RegisterLogin.Config;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -21,5 +25,21 @@ public class WebConfig implements WebMvcConfigurer{
         .allowedOrigins(allowedOrigins)
         .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
         .maxAge(MAX_AGE_SECS);
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	registry.addResourceHandler("/**")
+    			.addResourceLocations("classpath:/static/");
+    	exposeDirectory("upload", registry);
+    }
+    
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+    	Path uploadDir = Paths.get(dirName);
+    	String uploadPath = uploadDir.toFile().getAbsolutePath();
+    	
+    	if(dirName.startsWith("../")) dirName = dirName.replace("../", "");
+    	
+    	registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:///" + uploadPath + "/");
     }
 }
