1bc6e9a922e29486df96249c04c6be5696dab75d
vuxquang89 <vux.quang89@gmail.com>
2023-07-06 14:34:19 +0700

    add file QRItemRequest

---
+++ src/main/java/com/vux/example/RegisterLogin/Payload/Request/QRItemRequest.java
@@ -0,0 +1,21 @@
+package com.vux.example.RegisterLogin.Payload.Request;
+
+public class QRItemRequest {
+
+	private long id;
+	private String uri;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	
+}
